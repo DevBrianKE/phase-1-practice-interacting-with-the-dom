c50f9d84d4c618330dfb0edf10920263ecb68d9a
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-07-19 11:33:26 +0300

    added event listener to plus button

--- js/challenge.js
+++ js/challenge.js
@@ -15,3 +15,31 @@ const startTimer = () => {
 
 //start the timer when the page loads
 let interval = startTimer()
+
+
+//getting refrences to the buttons and comment form by there IDs for use in event listeners
+const minus = document.getElementById("minus")
+const plus = document.getElementById("plus")
+const heart = document.getElementById("heart")
+const pause = document.getElementById("pause")
+const commentForm = document.getElementById("comment-form")
+
+//adding event listener for minus button
+minus.addEventListener("click",() => {
+    //get the conuter element by ID
+    const counter = document.getElementById("counter")
+    //convert the counter's text to an interger
+    const count = parseInt(counter.innerText)
+    //decrement the counter;s value by 1 and update the displayed text
+    counter.innerText = count - 1
+})
+
+//adding event listener for plus button
+plus.addEventListener("click", () => {
+    //get the counter by its ID
+    const counter = document.getElementById("counter")
+    //convert the counter's text to an integer
+    const count = parseInt(counter.innerText)
+    //increment the counter's value by1 and update the displayed text
+    counter.innerText = count + 1
+})
