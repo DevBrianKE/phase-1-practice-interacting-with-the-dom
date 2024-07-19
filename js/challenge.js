008bf209494773aca2e1ffd8f10c977a5c0df6a8
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-07-19 11:00:53 +0300

    added timer

---
+++ js/challenge.js
@@ -0,0 +1,17 @@
+// Enforce strict mode to catch common mistakes
+"use strict"; 
+
+//fuction to start the timer
+const starttimer = () => {
+    return setInterval(() => {
+        //Get the counter element by id
+        const counter = document.getElementById("counter")
+        //Convert the counter's text to an integer
+        const count = parseInt(counter.innerText)
+        //Increment the counter by 1 and and update the displayed text
+        counter.innerText = count + 1
+    }, 1000) //execute every 1 seond
+};
+
+//start the timer when the page loads
+let interval = starttimer
